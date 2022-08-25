e8880e793226f0fce102de134023b9e4e85006e6
TranVux <vutran789jjjj@gmail.com>
2022-08-25 17:17:21 +0700

    add layout with express-handlebars

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const morgan = require('morgan');
+const path = require('path');
+const express = require('express')
+const hanldebars = require('express-handlebars');
+const app = express()
+const port = 3000
+
+// debug requests
+app.use(morgan('combined'))
+// end debug requests
+
+app.engine('hbs', hanldebars.engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
